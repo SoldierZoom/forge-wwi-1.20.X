e22b34722373494c84698f6134405ddd542fc29f
SoldierZoom <William@bradley.org>
2023-07-16 21:39:29 +0100

    Adding gemstones and recipes for sapphire materials

--- src/main/java/net/will/wwi/item/ModItems.java
+++ src/main/java/net/will/wwi/item/ModItems.java
@@ -14,6 +14,28 @@ public class ModItems {
     public static final RegistryObject<Item> FRANCIUM_INGOT = ITEMS.register("francium_ingot",
             () -> new Item(new Item.Properties()));
 
+    //gemstones
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PINK_TOURMALINE = ITEMS.register("pink_tourmaline",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TOPAZ = ITEMS.register("topaz",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> PINK_TOURMALINE_POWDER = ITEMS.register("pink_tourmaline_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RUBY_POWDER = ITEMS.register("ruby_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE_POWDER = ITEMS.register("sapphire_powder",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> TOPAZ_POWDER = ITEMS.register("topaz_powder",
+            () -> new Item(new Item.Properties()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
